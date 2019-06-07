640fa116c08510355032fc2bb3edc6a62955898b
yaame <jie.zhu@sbibits.com>
2019-06-07 01:57:56 +0000

    remove(lnbridge): event_handler fee_estimator rpc_client

--- src/lnbridge/mod.rs
+++ src/lnbridge/mod.rs
@@ -1,10 +1,6 @@
 pub mod key;
-
-// pub mod event_handler;
 pub mod utils;
 pub mod channel_monitor;
 pub mod channel_manager;
 pub mod log_printer;
-pub mod rpc_client;
-mod fee_estimator;
 mod broadcaster;
