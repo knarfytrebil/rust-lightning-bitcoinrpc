3454d98e44b3850c3343e1451337d5021fe83a80
Relaed <knarfytrebil@gmail.com>
2019-07-16 18:45:27 +0900

    (wip) 'staitc

--- server/src/ln_cmd/tasks/mod.rs
+++ server/src/ln_cmd/tasks/mod.rs
@@ -1,6 +1,6 @@
 use futures::future;
-use futures::{Poll, Async};
 use futures::future::Future;
+use futures::{Async, Poll};
 use ln_manager::executor::Larva;
 
 #[derive(Clone)]
@@ -22,10 +22,10 @@ impl Future for Action {
     type Error = ();
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        println!("Done: {}", self.done);  
+        println!("Done: {}", self.done);
         match self.done {
             true => Ok(Async::Ready(())),
-            false => Ok(Async::NotReady)
+            false => Ok(Async::NotReady),
         }
     }
 }
@@ -33,7 +33,7 @@ impl Future for Action {
 impl Larva for Probe {
     fn spawn_task(
         &self,
-        task: impl Future<Item = (), Error = ()> + Send,
+        task: impl Future<Item = (), Error = ()> + Send + 'static,
     ) -> Result<(), futures::future::ExecuteError<Box<dyn Future<Item = (), Error = ()> + Send>>>
     {
         Ok(())
