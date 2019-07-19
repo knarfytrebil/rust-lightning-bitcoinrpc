e536368249557c410e94e82a9cf8f57a02b9561b
Relaed <knarfytrebil@gmail.com>
2019-07-19 11:14:40 +0900

    (wip)

--- server/src/ln_cmd/tasks/mod.rs
+++ server/src/ln_cmd/tasks/mod.rs
@@ -86,7 +86,7 @@ impl Future for Action {
 impl Larva for Probe {
     fn spawn_task(
         &self,
-        mut task: impl Future<Item = (), Error = ()> + Send + 'static,
+        mut task: impl Future<Item = (), Error = ()> + Send,
     ) -> Result<(), futures::future::ExecuteError<Box<dyn Future<Item = (), Error = ()> + Send>>>
     {
         match self.async {
