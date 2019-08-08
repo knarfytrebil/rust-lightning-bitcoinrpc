88c0f2f290e195adbc44bfc3a5f2b57139974fed
Relaed <knarfytrebil@gmail.com>
2019-08-08 16:06:38 +0900

    (fix) format

--- server/src/ln_cmd/utils/node_info.rs
+++ server/src/ln_cmd/utils/node_info.rs
@@ -3,6 +3,8 @@ use secp256k1::key::PublicKey;
 use ln_manager::ln_bridge::utils::hex_str;
 
 pub fn get(node_secret: &secp256k1::key::SecretKey ) -> String {
-    hex_str(&PublicKey::from_secret_key(&Secp256k1::new(), node_secret).serialize())
-    // String::from(format!("{}\n{}", s1, s2))
+    String::from(format!(
+        "{} - Node Id",
+        hex_str(&PublicKey::from_secret_key(&Secp256k1::new(), node_secret).serialize())
+    ))
 }
