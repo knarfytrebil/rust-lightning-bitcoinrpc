4517d7d5eb235d37ebdad08ff240991a4bfc7c40
yaame <969519545@qq.com>
2019-09-20 04:26:24 +0000

    remove useless import

--- ln-manager/src/ln_cmd/peer.rs
+++ ln-manager/src/ln_cmd/peer.rs
@@ -3,12 +3,9 @@ use crate::ln_bridge::connection::{Connection, SocketDescriptor};
 use crate::ln_bridge::utils::{hex_str, hex_to_compressed_pubkey};
 
 use std::sync::Arc;
-use std::time::Duration;
 use std::net::ToSocketAddrs;
 use futures::channel::mpsc;
 use crate::executor::Larva;
-use futures::future;
-use futures::FutureExt;
 
 pub trait PeerC {
     fn connect(&self, node: String);
@@ -27,7 +24,6 @@ pub fn connect<T: Larva>(
     match hex_to_compressed_pubkey(node.split_at(0).1) {
         Some(pk) => {
             if node.as_bytes()[33 * 2] == '@' as u8 {
-                // let parse_res: Result<std::net::SocketAddr, _> = node.split_at(33 * 2 + 1).1.parse();
                 let str_node = node.split_at(33 * 2 + 1).1;
                 let parse_res = str_node.to_socket_addrs().unwrap().next();
                 if let Some(addr) = parse_res {
